3c53ba436ee3dc6a2b1913fba85d885de16bc6e6
blurryface17 <curvepiece@gmail.com>
2022-12-03 14:23:36 +0400

    Divided models on entities and DTO

---
+++ OnlineStore.Domain/Account.cs
@@ -0,0 +1,25 @@
+﻿using OnlineStore.Models;
+
+namespace OnlineStore.Domain;
+
+public record Account : IEntity
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+#pragma warning disable CS8618
+    private Account()
+#pragma warning restore CS8618
+    {
+    }
+
+    public Account(Guid id, string name, string email, string password)
+    {
+        Id = id;
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Email = email ?? throw new ArgumentNullException(nameof(email));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
+    }
+}
---
+++ OnlineStore.HttpModels/Requests/RegisterRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Models.Requests;
+
+public class RegisterRequest
+{
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    
+    [Required]
+    [MinLength(8)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+    public string ConfirmPassword { get; set; }
+    
+}
