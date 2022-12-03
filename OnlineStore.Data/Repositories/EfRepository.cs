8e2305f801b16ecece1a0700db809215627f9949
blurryface17 <curvepiece@gmail.com>
2022-12-03 14:26:27 +0400

    Reformatted Code

--- OnlineStore.Data/Repositories/EfRepository.cs
+++ OnlineStore.Data/Repositories/EfRepository.cs
@@ -6,13 +6,14 @@ namespace OnlineStore.Data.Repositories;
 public class EfRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
 {
     protected readonly AppDbContext DbContext;
+
     public EfRepository(AppDbContext dbContext)
     {
         DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
     }
 
     protected DbSet<TEntity> Entities => DbContext.Set<TEntity>();
-    
+
     public virtual async Task<TEntity> GetById(Guid id, CancellationToken cts = default)
     {
         var product = await Entities.FirstAsync(it => it.Id == id, cts);
@@ -31,7 +32,7 @@ public class EfRepository<TEntity> : IRepository<TEntity> where TEntity : class,
         await Entities.AddAsync(entity, cts);
         await DbContext.SaveChangesAsync(cts);
     }
-    
+
     public virtual async Task Update(TEntity entity, CancellationToken cts = default)
     {
         if (entity == null) throw new ArgumentNullException(nameof(entity));
--- OnlineStore.Data/Repositories/IRepository.cs
+++ OnlineStore.Data/Repositories/IRepository.cs
@@ -2,9 +2,9 @@
 
 public interface IRepository<TEntity>
 {
-    Task<TEntity> GetById(Guid id,CancellationToken cts = default);
+    Task<TEntity> GetById(Guid id, CancellationToken cts = default);
     Task<IReadOnlyList<TEntity>> GetAll(CancellationToken cts = default);
-    Task Add(TEntity entity,CancellationToken cts = default);
-    Task Update(TEntity entity,CancellationToken cts = default);
+    Task Add(TEntity entity, CancellationToken cts = default);
+    Task Update(TEntity entity, CancellationToken cts = default);
     Task DeleteById(Guid id, CancellationToken cts = default);
 }
