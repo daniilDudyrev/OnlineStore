a73ee1d2746bf2cc37b3f8e856dfb57b967f3d33
blurryface17 <curvepiece@gmail.com>
2022-12-08 16:17:13 +0400

    Initial Commit

---
+++ OnlineStore.Domain/Services/AccountService.cs
@@ -0,0 +1,37 @@
+﻿using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.RepositoryInterfaces;
+
+namespace OnlineStore.Domain.Services;
+
+public class AccountService
+{
+    private readonly IAccountRepository _accountRepository;
+
+    public AccountService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository ?? throw new ArgumentNullException(nameof(accountRepository));
+    }
+    public virtual async Task<Account> Register(string name, string email, string password, CancellationToken cts)
+    {
+        if (name == null) throw new ArgumentNullException(nameof(name));
+        if (email == null) throw new ArgumentNullException(nameof(email));
+        if (password == null) throw new ArgumentNullException(nameof(password));
+        var existedAccount = await _accountRepository.FindByEmail(email, cts);
+        var emailRegistered = existedAccount is not null;
+        if (emailRegistered)
+            throw new EmailExistsException("Email уже зарегистрирован", email);
+        var account = new Account(Guid.NewGuid(), name, email, password);
+        await _accountRepository.Add(account, cts);
+        return account;
+    }
+}
+[Serializable]
+public class EmailExistsException : Exception
+{
+    private string _email;
+
+    public EmailExistsException(string message, string email): base(message)
+    {
+        _email = email;
+    }
+}
