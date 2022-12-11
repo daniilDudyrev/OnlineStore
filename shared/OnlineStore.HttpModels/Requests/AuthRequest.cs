aced5fa66252ba093266a6539f32b4ecf587da48
blurryface17 <curvepiece@gmail.com>
2022-12-11 12:27:00 +0400

    Added Authentication Request

---
+++ shared/OnlineStore.HttpModels/Requests/AuthRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Models.Requests;
+
+public class AuthRequest
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(8)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
