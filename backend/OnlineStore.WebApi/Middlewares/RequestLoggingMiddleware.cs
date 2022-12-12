2bbb2e5ff2e2cec91bff5f76cff5e2083716b578
blurryface17 <curvepiece@gmail.com>
2022-12-12 15:33:15 +0400

    Added custom Middleware

---
+++ backend/OnlineStore.WebApi/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineStore.WebApi.Middlewares;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+    public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Response.HasStarted)
+        {
+            var requestPath = context.Request.Path;
+            await context.Response.WriteAsync(requestPath);
+            // _logger.LogInformation(requestPath);
+            await _next(context);
+        }
+    }
+}
