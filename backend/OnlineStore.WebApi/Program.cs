5c2e6c64fa6155859df24eee16523a5fb4d35136
blurryface17 <curvepiece@gmail.com>
2022-12-12 15:33:46 +0400

    Added LoggingFields and Middleware

--- backend/OnlineStore.WebApi/Program.cs
+++ backend/OnlineStore.WebApi/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
 using OnlineStore.Data;
 using OnlineStore.Data.Repositories;
 using OnlineStore.Domain.RepositoryInterfaces;
 using OnlineStore.Domain.Services;
+using OnlineStore.WebApi.Middlewares;
 using OnlineStore.WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,13 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 builder.Services.AddScoped<AccountService>();
 builder.Services.AddSingleton<IPasswordHasherService, Pbkdf2PasswordHasher>();
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.RequestHeaders
+                            | HttpLoggingFields.ResponseHeaders
+                            | HttpLoggingFields.RequestBody
+                            | HttpLoggingFields.ResponseBody;
+});
 
 const string dbPath = "myapp.db";
 builder.Services.AddDbContext<AppDbContext>(
@@ -22,10 +31,25 @@ builder.Services.AddDbContext<AppDbContext>(
 builder.Services.AddCors();
 var app = builder.Build();
 
+// app.Use(async (context, next) =>
+// {
+//     var userAgent = context.Request.Headers.UserAgent.ToString();
+//     if (userAgent.Contains("Edg"))
+//     {
+//         await next();
+//     }
+//     else
+//     {
+//         await context.Response.WriteAsJsonAsync(new { Message = "Only Edge is supported." });
+//     }
+// });
+app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseHttpsRedirection();
+app.UseHttpLogging();
 app.UseCors(policy =>
 {
     policy
-        .WithOrigins("http://localhost:7079","http://localhost:5050")
+        .WithOrigins("http://localhost:7079", "http://localhost:5050")
         .AllowAnyMethod()
         .AllowAnyHeader();
 });
@@ -35,7 +59,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 app.MapControllers();
