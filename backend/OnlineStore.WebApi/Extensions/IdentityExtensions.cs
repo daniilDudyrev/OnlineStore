4f357d358727e624647caa24153a4bdb03832f58
blurryface17 <curvepiece@gmail.com>
2022-12-19 00:11:45 +0400

    Added IdentityExtensions

---
+++ backend/OnlineStore.WebApi/Extensions/IdentityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace OnlineStore.WebApi.Extensions;
+
+public static class PrincipalExtensions
+{
+    public static Guid GetAccountId(this ClaimsPrincipal principal)
+    {
+        var strId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        var guid = Guid.Parse(strId);
+        return guid;
+    }
+}
