dbdd1e5e2913fe3f824168c9636c06e5bf514719
blurryface17 <curvepiece@gmail.com>
2022-12-18 23:55:47 +0400

    Added ComponentBase

---
+++ frontend/OnlineStore.BlazorClient/Components/AppComponentBase.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using OnlineStore.HttpApiClient;
+#pragma warning disable CS8618
+
+namespace OnlineStore.BlazorClient.Components;
+
+public abstract class AppComponentBase: ComponentBase
+{
+    [Inject] protected IShopClient ShopClient { get; private set; }
+    [Inject] protected ILocalStorageService LocalStorage { get; private set; }
+
+    private bool IsTokenChecked { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        if (!IsTokenChecked)
+        {
+            IsTokenChecked = true;
+            var token = await LocalStorage.GetItemAsync<string>("token");
+            if (!string.IsNullOrEmpty(token))
+            {
+                ShopClient.SetAuthToken(token);
+            }
+        }
+    }
+}
