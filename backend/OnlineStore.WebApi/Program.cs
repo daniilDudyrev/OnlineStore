90ddba89d5047d4a8f8d142d3f28d6e3fe8155ab
Asafenko <asafenko1703@gmail.com>
2023-04-09 00:45:34 +0400

    initial commit

--- backend/OnlineStore.WebApi/Program.cs
+++ backend/OnlineStore.WebApi/Program.cs
@@ -12,123 +12,166 @@ using OnlineStore.WebApi.Filters;
 using OnlineStore.WebApi.Mappers;
 using OnlineStore.WebApi.Middlewares;
 using OnlineStore.WebApi.Services;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(options =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger(); 
+Log.Information("Starting up");
+try
 {
-    options.Filters.Add<AppExceptionFilter>();
-    options.Filters.Add<ApiKeyFilter>();
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers(options =>
     {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        options.Filters.Add<AppExceptionFilter>();
+        options.Filters.Add<ApiKeyFilter>();
     });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
         {
-            new OpenApiSecurityScheme
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
             {
-                Reference = new OpenApiReference
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description =
+                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer e23yJhbGciO_iJTQwZ8\"",
+            });
+    
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
                 {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
                 }
-            },
-            Array.Empty<string>()
-        }
+            });
+        });
+    
+    builder.Services.AddScoped<HttpModelsMapper>();
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+    builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+    builder.Services.AddScoped<ICartRepository, CartRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+    builder.Services.AddScoped<IParentCategoryRepository, ParentCategoryRepository>();
+    builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWorkEf>();
+    builder.Services.AddScoped<AccountService>();
+    builder.Services.AddScoped<ProductService>();
+    builder.Services.AddScoped<CartService>();
+    builder.Services.AddScoped<CategoryService>();
+    builder.Services.AddScoped<ParentCategoryService>();
+    builder.Services.AddScoped<OrderService>();
+    builder.Services.AddSingleton<IPasswordHasherService, Pbkdf2PasswordHasher>();
+    builder.Services.AddScoped<ITokenService, JwtTokenService>();
+    builder.Services.AddHttpLogging(options =>
+    {
+        options.LoggingFields = HttpLoggingFields.RequestHeaders
+                                | HttpLoggingFields.ResponseHeaders;
     });
-});
-
-builder.Services.AddScoped<HttpModelsMapper>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IAccountRepository, AccountRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IParentCategoryRepository, ParentCategoryRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWorkEf>();
-builder.Services.AddScoped<AccountService>();
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddScoped<CartService>();
-builder.Services.AddScoped<CategoryService>();
-builder.Services.AddScoped<ParentCategoryService>();
-builder.Services.AddScoped<OrderService>();
-builder.Services.AddSingleton<IPasswordHasherService, Pbkdf2PasswordHasher>();
-builder.Services.AddScoped<ITokenService, JwtTokenService>();
-builder.Services.AddHttpLogging(options =>
-{
-    options.LoggingFields = HttpLoggingFields.RequestHeaders
-                            | HttpLoggingFields.ResponseHeaders;
-});
-var jwtConfig = builder.Configuration
-    .GetSection("JwtConfig")
-    .Get<JwtConfig>()!;
-builder.Services.AddSingleton(jwtConfig);
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+    
+    var jwtConfig = builder.Configuration
+        .GetSection("JwtConfig")
+        .Get<JwtConfig>()!;
+    builder.Services.AddSingleton(jwtConfig);
+    builder.Services.AddAuthentication(options =>
         {
-            IssuerSigningKey = new SymmetricSecurityKey(jwtConfig.SigningKeyBytes),
-            ValidateIssuerSigningKey = true,
-            ValidateLifetime = true,
-            RequireExpirationTime = true,
-            RequireSignedTokens = true,
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(jwtConfig.SigningKeyBytes),
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                RequireExpirationTime = true,
+                RequireSignedTokens = true,
 
-            ValidateAudience = true,
-            ValidateIssuer = true,
-            ValidAudiences = new[] { jwtConfig.Audience },
-            ValidIssuer = jwtConfig.Issuer
-        };
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidAudiences = new[] { jwtConfig.Audience },
+                ValidIssuer = jwtConfig.Issuer
+            };
+        });
+    
+    builder.Services.AddAuthorization();
+    
+    const string dbPath = "myapp.db";
+    builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseSqlite($"Data Source={dbPath}"));
+
+    builder.Services.AddCors();
+
+    builder.Services.AddDbContext<AppDbContext>(
+        options => options
+            .UseSqlite($"Data Source={dbPath}")
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging()
+    );
+    
+    builder.Host.UseSerilog((ctx, conf) =>
+    {
+        conf
+            .MinimumLevel.Debug()
+            .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+            .WriteTo.Console(LogEventLevel.Information)
+            .ReadFrom.Configuration(ctx.Configuration);
     });
-builder.Services.AddAuthorization();
+    
+    var app = builder.Build();
 
+    app.UseStaticFiles();
 
-const string dbPath = "myapp.db";
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlite($"Data Source={dbPath}"));
+    app.UseSerilogRequestLogging();
+    
+    app.UseHttpsRedirection();
 
-builder.Services.AddCors();
-var app = builder.Build();
+    app.UseMiddleware<PageTransitionsMiddleware>();
+    app.UseHttpLogging();
 
-app.UseHttpsRedirection();
+    app.UseCors(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:7079", "http://localhost:5050")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+    
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseMiddleware<PageTransitionsMiddleware>();
-app.UseHttpLogging();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.UseCors(policy =>
-{
-    policy
-        .WithOrigins("http://localhost:7079", "http://localhost:5050")
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-});
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.Run();
+    
 }
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush(); //перед выходом дожидаемся пока все логи будут записаны
+}
 
 public partial class Program{}
